d15790e4e4322b5590c1b6a2d8079b6b6d6b45af
xdlhzdh <xdlhzdh@163.com>
2024-07-21 11:27:05 +0800

    Changes for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/qinjiangyuan-dashboard',
     images: {
         unoptimized: true,
     },
-    distDir: 'out', // Where to export all pages
-    trailingSlash: true,
-    assetPrefix: '.',
+    // distDir: 'out', // Where to export all pages
+    // trailingSlash: true,
+    // assetPrefix: '.',
 
 
-    // time in seconds of no pages generating during static
-    // generation before timing out
-    staticPageGenerationTimeout: 1000,
-    reactStrictMode: false
+    // // time in seconds of no pages generating during static
+    // // generation before timing out
+    // staticPageGenerationTimeout: 1000,
+    // reactStrictMode: false
 }
 
 export default nextConfig;
